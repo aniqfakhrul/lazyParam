132221b2844f180145ea9bfcdc559e4568eb3c38
jcermat <22714237+jcermat@users.noreply.github.com>
2021-02-03 15:24:40 +0800

    implemented multithreading support

--- lazyparam.py
+++ lazyparam.py
@@ -5,6 +5,8 @@ import sys
 import requests
 import re
 import time
+import threading
+from queue import Queue
 from cores.colors import green, white, end, info, bad, good, run, yellow, bold
 from cores.utils import get_random_string,decode,encode
 
@@ -12,7 +14,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-u',help='target url', dest='url')
 parser.add_argument('-w',help='wordlist path', dest='wordlist', default='./db/short_params.txt')
 parser.add_argument('-b','--cookie',help='cookie', dest='cookie')
+parser.add_argument('-t','--threads',help='number of threads', dest='num_threads', default='4')
 args = parser.parse_args()
+print_lock = threading.Lock()
+q = Queue()
+currentMethod = 'GET' # method for threads to refer to
+num_threads = int(args.num_threads) # default 8 threads
+values = ['../../../../../../../../etc/passwd', 'w'] # values to fuzz LFI, RCE
+bypass_char = '' # for threads to refer to
 
 url = args.url
 wordlist = args.wordlist
@@ -40,6 +49,7 @@ else:
                     'Upgrade-Insecure-Requests' : '1',
                 }
 
+foundParams = []
 paramList = []
 try:
     with open(wordlist, 'r', encoding="utf8") as file:
@@ -81,62 +91,78 @@ def vulnerable(response, vuln):
         else:
             return False
 
-def checkParams(response, url, headers, bypass_char):
+# checkParams iteration wrapped in one function
+def checkUrlParams(url, param, method, values, originalLength):
     breaker_rce = False
     breaker_lfi = False
-    values = ['../../../../../../../../etc/passwd', 'w']
+    #method = currentMethod
+    for value in values:
+        value = '%s%s' % (value,bypass_char)
+        data = {param:value}
+        response = requester(url=url, method=method, data=data, headers=headers)
+        if len(response.text) != originalLength: # Found!
+            if value == 'w': #  RCE Found
+                if vulnerable(response.text, vuln="rce"):
+                    with print_lock:
+                        print("%s Found valid param: %s%s %s%s(RCE!)%s"  % (good, green,param,bold,yellow,end))
+                        foundParams.insert(0, param)
+                        breaker_rce = True
+                else:
+                    with print_lock:
+                        print("%s Found valid param (This might be false positive): %s%s%s" % (info, green,param,end))
+            elif value != 'w': #LFI Found
+                if vulnerable(response.text, vuln="lfi"):
+                    with print_lock:
+                        print("%s Found valid param: %s%s%s (%s?%s=%s)"  % (good,green,param,end,url,param,value))
+                        foundParams.insert(0, param)
+                        breaker_lfi = True
+                else:
+                    with print_lock:
+                        print("%s Found valid param (This might be false positive): %s%s%s" % (info, green,param,end))
+    with print_lock:
+        print("%s Trying: %s" % (info,param), end="\r", flush=True)
+    #return breaker_lfi, breaker_rce
+
+def threader(originalLength):
+    while True:
+        param = q.get()
+        checkUrlParams(url, param, currentMethod, values, originalLength)
+        q.task_done()
+
+def checkParams(response, url, headers):
+    global currentMethod
+    currentMethod = 'GET'
+    breaker_rce = False
+    breaker_lfi = False
     originalLength = len(response.text)
     # check for GET method
     print("\n%s Checking for GET request..." % good)
     for index, param in enumerate(paramList):
-        if breaker_lfi and breaker_rce:
-            break
-        for value in values:
-            value = '%s%s' % (value,bypass_char)
-            data = {param:value}
-            response = requester(url=url, method='GET', data=data, headers=headers)
-            if len(response.text) != originalLength: # Found!
-                if value == 'w' and not breaker_rce: #  RCE Found
-                    if vulnerable(response.text, vuln="rce"):
-                        print("%s Found valid param: %s%s %s%s(RCE!)%s"  % (good, green,param,bold,yellow,end))
-                        foundParams.insert(0, param)
-                        breaker_rce = True
-                    else:
-                        print("%s Found valid param (This might be false positive): %s%s%s" % (info, green,param,end))
-                elif value != 'w' and not breaker_lfi: #LFI Found
-                    if vulnerable(response.text, vuln="lfi"):
-                        print("%s Found valid param: %s%s%s (%s?%s=%s)"  % (good,green,param,end,url,param,value))
-                        foundParams.insert(0, param)
-                        breaker_lfi = True
-                    else:
-                        print("%s Found valid param (This might be false positive): %s%s%s" % (info, green,param,end))
-        print("%s Trying: %s" % (info,param), end="\r", flush=True)
+        # temp disable - difficult to orchestrate
+        #if breaker_lfi and breaker_rce:
+        #    break
+
+        # multithreading implementation
+        # add words to queue
+        q.put(param)
+    q.join()
+
     
     # check for POST method
+    currentMethod = 'POST'
     breaker_rce = False
     breaker_lfi = False
     print("%s Checking for POST request..." % good)
     for param in paramList:
-        for value in values:
-            value = '%s%s' % (value,bypass_char)
-            data = {param:value}
-            response = requester(url=url, method='POST', data=data, headers=headers)
-            if len(response.text) != originalLength:
-                if value == 'w' and not breaker_rce: #  RCE Found
-                    if vulnerable(response.text, vuln="rce"):
-                        print("%s Found valid param: %s%s %s%s(RCE!)%s"  % (good, green,param,bold,yellow,end))
-                        foundParams.insert(0, param)
-                        breaker_rce = True
-                    else:
-                        print("%s Found valid param %s%s(RCE!)%s (might be false positive): %s%s%s" % (info, bold,yellow,end, green,param,end))
-                elif value != 'w' and not breaker_lfi: #LFI Found
-                    if vulnerable(response.text, vuln="lfi"):
-                        print("%s Found valid param: %s%s%s (%s?%s=%s)"  % (good,green,param,end,url,param,value))
-                        foundParams.insert(0, param)
-                        breaker_lfi = True
-                    else:
-                        print("%s Found valid param (This might be false positive): %s%s%s" % (info, green,param,end))
-        print("%s Trying: %s" % (info,param), end="\r", flush=True)
+        # temp disable - difficult to orchestrate
+        #if breaker_lfi and breaker_rce:
+        #    break
+
+        # multithreading implementation
+        # add words to queue
+        q.put(param)
+    q.join()
+
     return foundParams
 
 def intensive(response, url, headers):
@@ -147,13 +173,13 @@ def intensive(response, url, headers):
             bypass_chars.append(line.strip())
     for char in bypass_chars:
         print("%s Trying with %s" % (info,char))
-        checkParams(response,url,headers, char)
+        bypass_char = char
+        checkParams(response, url, headers)
 
 
 
 
 if __name__ == "__main__":
-    foundParams = []
     finalResult = []
     bypassed_chars = []
     RCE = False
@@ -169,9 +195,17 @@ if __name__ == "__main__":
                 # parse_webpage
                 print("%s Parsing webpage for potential parameters..." % good)
                 parse(response.text)
+                originalLength = len(response.text)
+                # initialize threads
+                for x in range(num_threads):
+                    t = threading.Thread(target=threader, args=(originalLength,))
+                    #t.daemon = True
+                    t.start()
+                print("\n%s Running with %d threads" % (info,num_threads))
                 # lfi and rce checking
                 start_time = time.time() # Start execution time
-                checkParams(response, url, headers, bypass_char='')
+                checkParams(response, url, headers)
+                print(foundParams)
                 if not foundParams:
                     print("%s No parameter found, trying bypassing techniques..." % info)
                     # bypassed_chars = checkParams()
